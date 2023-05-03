9e76f106a8aad61de010c1d02deaa48bc2c1cdfc
Lucio <lpgn@users.noreply.github.com>
2023-05-03 12:26:20 +0100

    cleanup

--- firmware/Incubator/data/src/context/incubator/incubatorContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-const incubatorContext = createContext();
-
-export default incubatorContext;
--- firmware/Incubator/data/src/context/incubator/types.js
+++
@@ -1,3 +0,0 @@
-export const SET_LOADING = "SET_LOADING";
-export const STOP_LOADING = "STOP_LOADING";
-export const GET_INCUBATORS = "GET_INCUBATORS";
