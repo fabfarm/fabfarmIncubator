9d24d0b655929ca92be5780b9634290450e7c7d4
NoHara42 <mailned42@gmail.com>
2023-05-18 19:48:45 +0200

    format main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,36 +1,34 @@
 // Fabfarm Egg Incubator
 // Written by: Lucio
 
-#include "WebServerManager.h"
-#include "config.h"
-#include "FileManager.h"
-#include "WifiManagerHelper.h"
+#include "DebugManager.h"
 #include "DisplayManager.h"
+#include "FileManager.h"
 #include "SensorManager.h"
 #include "ServoManager.h"
-#include "DebugManager.h"
-#include "runIncubator.h"
+#include "WebServerManager.h"
 #include "WifiManagerHelper.h"
-
+#include "config.h"
+#include "runIncubator.h"
 
 void setup() {
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-  wifiManagerSetup();
-  AsyncElegantOTA.begin(&server);
-  Serial.begin(115200);
-  initializeTFTDisplay();
-  initializeStorage();
-  loadSettings();
-  initializeWebServer();
-  connectServos();
-  initializeSensors();
-  debugMessage("Setup complete");
-  pinMode(mosfetPin, OUTPUT);
+    WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
+    wifiManagerSetup();
+    AsyncElegantOTA.begin(&server);
+    Serial.begin(115200);
+    initializeTFTDisplay();
+    initializeStorage();
+    loadSettings();
+    initializeWebServer();
+    connectServos();
+    initializeSensors();
+    debugMessage("Setup complete");
+    pinMode(mosfetPin, OUTPUT);
 }
 
 void loop() {
-  wifiManagerTrigerPinSetup();
-  runIncubator();
+    wifiManagerTrigerPinSetup();
+    runIncubator();
 }
 
-//last time I checked we had 430 lines of code now 387
+// last time I checked we had 430 lines of code now 387
