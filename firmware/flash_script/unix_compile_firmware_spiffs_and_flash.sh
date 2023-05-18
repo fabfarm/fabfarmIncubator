6a74bf12a45749ea9d368e6c641977102fe51ee7
NoHara42 <mailned42@gmail.com>
2023-05-18 15:11:00 +0200

    correct order of .sh script to stop error

--- firmware/flash_script/unix_compile_firmware_spiffs_and_flash.sh
+++ firmware/flash_script/unix_compile_firmware_spiffs_and_flash.sh
@@ -9,6 +9,9 @@
 pip3 install esptool
 
 while true; do
+  # run PlatformIO build for T-QT-Pro-N4R2 environment
+  platformio run --environment T-QT-Pro-N4R2
+
   # run PlatformIO build target for building file system image
   platformio run --target buildfs --environment T-QT-Pro-N4R2
 
@@ -16,9 +19,6 @@ while true; do
   # replace this with your own path to esptool
   # (use your own python package manager like conda, or `pip install esptool` then `python -m esptool ...`)
   esptool.py --chip esp32s3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0 ./.pio/build/T-QT-Pro-N4R2/bootloader.bin 0x8000 ./.pio/build/T-QT-Pro-N4R2/partitions.bin 0xe000 ./flash_script/boot_app0.bin 0x10000 ./.pio/build/T-QT-Pro-N4R2/firmware.bin 0x00290000 ./.pio/build/T-QT-Pro-N4R2/spiffs.bin
-
-  # run PlatformIO build for T-QT-Pro-N4R2 environment
-  platformio run --environment T-QT-Pro-N4R2
 
   # monitor device using PlatformIO
   platformio device monitor --environment T-QT-Pro-N4R2 --port /dev/ttyACM0
