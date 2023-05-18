fa2ca987f3ed5b9e14629b8162309ec3338076da
NoHara42 <mailned42@gmail.com>
2023-05-18 15:47:12 +0200

    format and add readFile function to FileManager

--- firmware/src/FileManager.h
+++ firmware/src/FileManager.h
@@ -2,11 +2,14 @@
 #define FILEMANAGER_H
 
 #include <Arduino.h>
+#include <FS.h>
 
-void    initializeStorage();
+void initializeStorage();
 
-void    writeToFile(const char *fileName, const String &content, bool append = false);
+void writeToFile(const char *fileName, const String &content,
+                 bool append = true);
 
-String  readFromFile(const char *fileName);
+String   readFromFile(const char *fileName);
+fs::File readFile(const char *fileName);
 
 #endif
